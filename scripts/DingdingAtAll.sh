2860d54dc3769f71a3bb19ff0476752b3cbaebee
happinesslijian <47111417+happinesslijian@users.noreply.github.com>
2022-08-16 12:28:02 +0800

    Update DingdingAtAll.sh

--- scripts/DingdingAtAll.sh
+++ scripts/DingdingAtAll.sh
@@ -5,7 +5,7 @@ Linkingnic=`nmcli con show --active | egrep -v 'docker|br-|lo|veth' | tail -n1 |
 Mac=`ifconfig $Linkingnic | awk 'NR==4{print $2}'`
 echo -e 使用人是:$User 资产编码是:$Assetcode Mac地址是:$Mac
 
-curl 'https://oapi.dingtalk.com/robot/send?access_token=317477bc611531630b8f4336506b323a298728c81898734861bf24d7d9a28026' \
+curl 'https://oapi.dingtalk.com/robot/send?access_token=317477bc611531630b8f4336506b323a298728c81898734861bf24d7d9axxxxx' \
 	-H 'Content-Type: application/json' \
 	-d "{\"msgtype\": \"text\", 
 	     \"text\": {\"content\": \"使用人是:$User 资产编码是:$Assetcode Mac地址是:$Mac\"},
