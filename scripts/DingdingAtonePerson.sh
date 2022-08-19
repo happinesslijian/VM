c82a221aa53ba148a489f43c00d356359d77c56d
happinesslijian <47111417+happinesslijian@users.noreply.github.com>
2022-08-19 09:50:04 +0800

    Update DingdingAtonePerson.sh

--- scripts/DingdingAtonePerson.sh
+++ scripts/DingdingAtonePerson.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-read -r -p "输入使用人： " User
-read -r -p "输入资产编码： " Assetcode
-Linkingnic=`nmcli con show --active | egrep -v 'docker|br-|lo|veth' | tail -n1 | awk '{print $6}'`
-Mac=`ifconfig $Linkingnic | awk 'NR==4{print $2}'`
-echo -e 使用人是:$User 资产编码是:$Assetcode Mac地址是:$Mac
+UUID=`nvidia-smi -q | grep -i 'gpu uuid' | awk '{print $4}' | awk -F 'GPU-' '{print $2}'`
+NIC=`nmcli con show --active | egrep -v 'docker|br-|lo|veth' | tail -n1 | awk '{print $6}'`
+IP=`ifconfig $NIC | awk 'NR==2{print $2}'`
 
-curl 'https://oapi.dingtalk.com/robot/send?access_token=b9574a51821f6f1a2ad1pls689522b8edcvfa5b308730a61627d5580bfXXXXXX' \
-	-H 'Content-Type: application/json' \
-	-d "{\"msgtype\": \"text\", 
-	     \"text\": {\"content\": \"使用人是:$User 资产编码是:$Assetcode Mac地址是:$Mac\"},
-	     \"at\": {\"atMobiles\": [\"151XXXXXXXX\"]}}"
+echo -e IP地址是：$IP UUID是：$UUID
+curl 'https://oapi.dingtalk.com/robot/send?access_token=317778bc611531plkh8f4336506b323a298728c81898734861bf24d7d9axxx26' \
+		-H 'Content-Type: application/json' \
+		-d "{\"msgtype\": \"text\",
+	     	     \"text\": {\"content\": \"IP地址是：$IP UUID是：\r$UUID\r\"},
+		     \"at\": {\"atMobiles\":[151xxxxxxxx]}}"
+
